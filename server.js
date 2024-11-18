8f3f4d712819a221028c23930d7c23f0b39d8bb5
HoolAgain <evallis1867@gmail.com>
2024-11-18 08:12:42 -0500

    database adjustments made. Removal of users and addition of greetings

--- server.js
+++ server.js
@@ -4,9 +4,38 @@ const sqlite3 = require('sqlite3');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
+const greetingsData = [
+    ['Morning', 'English', 'Good morning!', 'Formal'],
+    ['Afternoon', 'English', 'Good afternoon!', 'Formal'],
+    ['Evening', 'English', 'Good evening!', 'Formal'],
+    ['Morning', 'English', 'Mornin\'!', 'Casual'],
+    ['Afternoon', 'English', 'Afternoon!', 'Casual'],
+    ['Evening', 'English', 'Evenin\'!', 'Casual'],
+    ['Morning', 'French', 'Bonjour!', 'Formal'],
+    ['Afternoon', 'French', 'Bon après-midi!', 'Formal'],
+    ['Evening', 'French', 'Bon soiree!', 'Formal'],
+    ['Morning', 'French', 'Salut!', 'Casual'],
+    ['Afternoon', 'French', 'Bon après.', 'Casual'],
+    ['Evening', 'French', 'Ouais Demain', 'Casual'],
+    ['Morning', 'Spanish', 'Buen dia!', 'Formal'],
+    ['Afternoon', 'Spanish', 'Buenas tardes!', 'Formal'],
+    ['Evening', 'Spanish', 'Buenas noches!', 'Formal'],
+    ['Morning', 'Spanish', 'Manana!', 'Casual'],
+    ['Afternoon', 'Spanish', 'La tarde.', 'Casual'],
+    ['Evening', 'Spanish', 'Cya manana!', 'Casual']
+];
 
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+});
 
 // Open the SQLite database
 let db;
@@ -16,14 +45,28 @@ let db;
         driver: sqlite3.Database
     });
 
-    // Create a 'users' table if it doesn't exist
+    //here in case of fire, push the red button and reset table
+    //await db.exec('DROP TABLE IF EXISTS greetings');
+
+    // Create a 'greetings' table if it doesn't exist
     await db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS greetings (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      email TEXT NOT NULL UNIQUE
+      timeOfDay TEXT NOT NULL,
+      language TEXT NOT NULL,
+      greetingMessage TEXT,
+      tone TEXT NOT NULL
     )
   `);
+
+    // Seed the 'greetings' table
+    const insertQuery = `
+        INSERT INTO greetings (timeOfDay, language, greetingMessage, tone)
+        VALUES (?, ?, ?, ?)`;
+
+    for (const greeting of greetingsData) {
+        await db.run(insertQuery, greeting);
+    }
 })();
 
 // GET all users
